689121cac88ffcf3d9868188acba47a7be7261fb
AarhamH <aarham.haider@gmail.com>
2025-03-26 03:43:45 -0700

    Implemented `NewBaseFile`

--- storage/base.go
+++ storage/base.go
@@ -1,16 +1,36 @@
 package storage
 
-import "sync/atomic"
+import (
+	"log"
+	"os"
+	"sync/atomic"
+)
 
 type Base struct {
-  id int
-  page_count *atomic.Uint32
+  id int 
+  page_count uint32
 }
 
 // can represent basefile id or file descriptor
 func (b Base) GetId() int { return b.id }
-func (b Base) GetPageCount() *atomic.Uint32 { return b.page_count }
-func (b Base) IncrementPageCount() { b.page_count.Add(1) }
+func (b Base) GetPageCount() uint32 { return b.page_count }
+func (b Base) IncrementPageCount() { atomic.AddUint32(&b.page_count, 1) }
+
+func NewBaseFile(fileName string) *Base {
+  file, err := os.OpenFile(fileName, os.O_TRUNC|os.O_RDWR|os.O_CREATE, 0664)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  b := Base{ id: int(file.Fd()), page_count: 0 }
+  
+  err = file.Close()
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  return &b;
+}
 
 
 
